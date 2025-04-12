4d01d9c2338215bddd91d3f438fec1e8b7943276
anggasastra <angga.punk20@gmail.com>
2025-04-12 22:39:43 +0800

    commit baru 6

--- index.js
+++ index.js
@@ -1,23 +1,4 @@
-const express = require('express');
-const db = require('./config/db');
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.json());
-
-// Tes koneksi dan route simple
-app.get('/', async (req, res) => {
-  try {
-    const [rows] = await db.query('SELECT 1');
-    res.send('Database connected!');
-  } catch (err) {
-    console.error('Database error:', err.message);
-    res.status(500).send('Database error: ' + err.message);
-  }
-});
-
-console.log("JWT_SECRET:", process.env.JWT_SECRET); // <== debug
+const app = require('./src/app');
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const db = require('./config/db');
 
 dotenv.config();
 
@@ -46,6 +47,20 @@ app.use((req, res, next) => {
     message: 'Endpoint tidak ditemukan'
   });
 });
+
+// Tes koneksi dan route simple
+const PORT = process.env.PORT || 3000;
+app.get('/', async (req, res) => {
+  try {
+    const [rows] = await db.query('SELECT 1');
+    res.send('Database connected!');
+  } catch (err) {
+    console.error('Database error:', err.message);
+    res.status(500).send('Database error: ' + err.message);
+  }
+});
+
+console.log("JWT_SECRET:", process.env.JWT_SECRET); // <== debug
 
 app.use(errorHandler);
 
