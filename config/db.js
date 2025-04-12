5c048256e76e5838a345045d1a23a9ee74d47442
anggasastra <angga.punk20@gmail.com>
2025-04-12 21:15:43 +0800

    commit baru

--- config/db.js
+++ config/db.js
@@ -1,8 +1,21 @@
 const mysql = require('mysql2/promise');
 const dotenv = require('dotenv');
+const { URL } = require('url');
 
 dotenv.config();
 
-const db = mysql.createPool(process.env.DB_URL);
+// Parse DB_URL
+const dbUrl = new URL(process.env.DB_URL);
 
-module.exports = db;
+const pool = mysql.createPool({
+  host: dbUrl.hostname,
+  user: dbUrl.username,
+  password: dbUrl.password,
+  database: dbUrl.pathname.substring(1),
+  port: dbUrl.port,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+module.exports = pool;
