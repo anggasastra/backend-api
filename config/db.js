3eb488f12efab30277996ec9c33adae8e6061010
anggasastra <angga.punk20@gmail.com>
2025-04-12 20:19:19 +0800

    commit

--- config/db.js
+++ config/db.js
@@ -1,11 +1,14 @@
-const mysql = require('mysql2/promise');
-require('dotenv').config();
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
 
-const db = mysql.createPool({
+dotenv.config();
+
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
-module.exports = db;
+module.exports = pool.promise();
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.DB_PORT;
 
 app.listen(PORT, () => {
   console.log(`✅ Server berjalan di http://localhost:${PORT}`);
